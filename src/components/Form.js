88f2b7e16b6290e96b2fd4c31170d15f638617af
fabriciodev28 <fabriciodev28@gmail.com>
2025-07-19 20:01:43 -0300

    Adicionando useState em formularios

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,44 @@
+import { useState } from "react"
+
 function Form(){
 
     function cadastrarUsuario(e){
         e.preventDefault()
-        console.log("Usuário cadastrado!")
+        console.log(`Usuário: ${name} entrou com a senha: ${password}`)
     }
 
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return(
         <>
         
-        <h1>Meu cadastro:</h1>
-        <form onSubmit={cadastrarUsuario}>
-            <div>
-                <input type="text" placeholder="Digite o nome"></input>
-            </div>
-            <div>
-                <input type="submit" value="Cadastrar"></input>
-            </div>
-        </form>
+            <h1>Meu cadastro:</h1>
+            <form onSubmit={cadastrarUsuario}>
+                <div>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    placeholder="Digite o nome" 
+                    onChange={(e)=> setName(e.target.value)}>
+                    </input>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Digite sua senha"
+                    onChange={(e)=> setPassword(e.target.value)}>
+                    </input>
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar"></input>
+                </div>
+            </form>
 
         </>
     )
