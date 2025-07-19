17717a66c2a7bf4366fb0880fd3d3a98f5cd156c
fabriciodev28 <fabriciodev28@gmail.com>
2025-07-19 19:06:20 -0300

    Criando propTypes required e defaultProps

--- src/components/List.js
+++ src/components/List.js
@@ -7,9 +7,11 @@ function List(){
         <>
         <h1>Minha lista</h1>
         <ul className={styles.lista}>
-            <Item marca="Ferrari"/>
-            <Item marca="BMW"/>
-            <Item marca="Audi"/>
+            <Item marca="Ferrari" ano_lancamento={1987}/>
+            <Item marca="BMW" ano_lancamento={1977}/>
+            <Item marca="Audi" ano_lancamento={2000}/>
+            <Item marca="Chevrolet" ano_lancamento={1980}/>
+            <Item/>
         </ul>
         
         </>
